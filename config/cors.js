52b5fbae927383128db2fd4ec52296f636976664
Allan Ayford <60179207+allanayford-dev@users.noreply.github.com>
2025-01-26 20:28:23 +0200

    Update cors links.

--- config/cors.js
+++ config/cors.js
@@ -7,8 +7,8 @@ const allowedOrigins = [
 	'http://localhost:8963',
 	'http://127.0.0.1:3001',
 	'http://127.0.0.1:3000',
-	'https://express-api-c0r1.onrender.com/',
-	'https://nextjs-frontend-xbkq.onrender.com/',
+	'https://express-api-c0r1.onrender.com', // Removed trailing slash
+	'https://nextjs-frontend-xbkq.onrender.com', // Removed trailing slash
 ]
 
 const corsOptions = {
@@ -16,25 +16,12 @@ const corsOptions = {
 		if (!origin || allowedOrigins.includes(origin)) {
 			callback(null, true)
 		} else {
-			callback(new Error('Not allowed by CORS'), false)
+			callback(new Error('Not allowed by CORS'))
 		}
 	},
 	credentials: true,
-	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 	allowedHeaders: ['Content-Type', 'Authorization'],
 }
 
-const corsTesting = {
-	origin: function (origin, callback) {
-		if (!origin || allowedOrigins.includes(origin)) {
-			callback(null, true)
-		} else {
-			callback(new Error('Not allowed by CORS'), false)
-		}
-	},
-	credentials: true,
-	methods: ['GET', 'POST', 'PUT', 'DELETE'],
-	allowedHeaders: ['Content-Type', 'Authorization'],
-}
-
-module.exports = NODE_ENV === 'development' ? corsTesting : corsOptions
+module.exports = corsOptions
